de3f8858f508ff65344dbd163c5e905fd3776ebe
NekoYasha <autornekoyasha@gmail.com>
2023-04-04 12:29:57 -0300

    added method comments

--- src/main/java/com/github/nekoyasha7/oopregistrationproject/service/StudentService.java
+++ src/main/java/com/github/nekoyasha7/oopregistrationproject/service/StudentService.java
@@ -37,24 +37,44 @@ public class StudentService {
 
     }
 
+    /**
+     *
+     * Search a student in the database
+     * @param id of the student to be searched.
+     * @return student with the specified ID, null otherwise.
+     */
     public Student findStudentById(int id){
 
         return studentDaoImpl.findById(id);
 
     }
 
-    public List<Student> listAllStudents(Student student){
+    /**
+     * List all students.
+     * @return a list with all students in the database.
+     */
+    public List<Student> listAllStudents(){
 
         return this.getStudentDaoImpl().listStudents();
 
     }
 
+    /**
+     *
+     * @param id of the student to be removed from the database.
+     * @return true if the student exists, false otherwise.
+     */
     public boolean removeStudent(int id){
 
         return this.getStudentDaoImpl().remove(id);
 
     }
 
+    /**
+     *
+     * @param student to be updated.
+     * @return true if the student exists in the database and the update was successful, false otherwise.
+     */
     public boolean updateStudent(Student student){
 
         return this.getStudentDaoImpl().update(student);
