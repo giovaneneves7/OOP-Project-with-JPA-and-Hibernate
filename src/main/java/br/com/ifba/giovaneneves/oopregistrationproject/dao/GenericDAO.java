24e56cbd380cb70573979ed39b69875dc3205d04
NekoYasha <autornekoyasha@gmail.com>
2023-04-06 12:25:20 -0300

    changed package names

---
+++ src/main/java/br/com/ifba/giovaneneves/oopregistrationproject/dao/GenericDAO.java
@@ -0,0 +1,2 @@
+package br.com.ifba.giovaneneves.oopregistrationproject.dao;public class GenericDAO {
+}
---
+++ src/main/java/br/com/ifba/giovaneneves/oopregistrationproject/model/AbstractEntity.java
@@ -0,0 +1,2 @@
+package br.com.ifba.giovaneneves.oopregistrationproject.model;public class AbstractEntity {
+}
