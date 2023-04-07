50355374b70f227d8ee0a613c4fba5eb9ebaf458
NekoYasha <autornekoyasha@gmail.com>
2023-04-07 11:03:55 -0300

    added static connectionFactory

--- src/main/java/br/com/ifba/giovaneneves/oopregistrationproject/dao/StudentDAOImpl.java
+++ src/main/java/br/com/ifba/giovaneneves/oopregistrationproject/dao/StudentDAOImpl.java
@@ -1,31 +1,12 @@
 package br.com.ifba.giovaneneves.oopregistrationproject.dao;
 
 //--+ Imports +--//
-import br.com.ifba.giovaneneves.oopregistrationproject.connection.ConnectionFactory;
 import br.com.ifba.giovaneneves.oopregistrationproject.model.Student;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 //--+ END Imports +--//
 
 public class StudentDAOImpl extends StudentDAO{
-    private ConnectionFactory connectionFactory;
-
-    public ConnectionFactory getConnectionFactory() {
-        return connectionFactory;
-    }
-
-    public void setConnectionFactory(ConnectionFactory connectionFactory) {
-        this.connectionFactory = connectionFactory;
-        connectionFactory.connect();
-    }
-
-    public StudentDAOImpl(){
-
-        this.setConnectionFactory(new ConnectionFactory());
-
-    }
-
 
     /**
      * List all students.
@@ -34,10 +15,7 @@ public class StudentDAOImpl extends StudentDAO{
     @Override
     public List<Student> listStudents(){
 
-        EntityManager entityManager = this.getConnectionFactory().getEntityManager();
-
-
-        return entityManager.createQuery("SELECT s FROM Student s", Student.class).getResultList();
+        return GenericDAO.getEntityManager().createQuery("SELECT s FROM Student s", Student.class).getResultList();
 
     }
 
