d7d597fd19fbb5cb98608ddd42fdf4a38e1409f0
NekoYasha <autornekoyasha@gmail.com>
2023-04-11 14:12:52 -0300

    added facade layer

--- src/main/java/br/com/ifba/giovaneneves/oopregistrationproject/controller/FacadeInstance.java
+++ src/main/java/br/com/ifba/giovaneneves/oopregistrationproject/controller/FacadeInstance.java
@@ -1,2 +1,35 @@
-package br.com.ifba.giovaneneves.oopregistrationproject.controller;public class FacadeInstance {
+package br.com.ifba.giovaneneves.oopregistrationproject.controller;
+
+public class FacadeInstance {
+
+    //----------------------------------{ ATTRIBUTES }----------------------------------//
+    private static IFacade facadeInstance;
+    private static final Object MONITOR = new Object();
+
+    //----------------------------------{ CONSTRUCTOR }----------------------------------//
+    public FacadeInstance(){
+
+        super();
+
+    }
+
+    //----------------------------------{ METHODS }----------------------------------//
+
+    /**
+     *
+     * @return A new instance of FacadeInstance if there's not.
+     */
+    public static IFacade getInstance(){
+
+        synchronized (MONITOR){
+
+            if(facadeInstance == null){
+                facadeInstance = new Facade();
+            }
+
+        }
+
+        return facadeInstance;
+
+    }
 }
--- src/main/java/br/com/ifba/giovaneneves/oopregistrationproject/controller/IFacade.java
+++ src/main/java/br/com/ifba/giovaneneves/oopregistrationproject/controller/IFacade.java
@@ -1,2 +1,17 @@
-package br.com.ifba.giovaneneves.oopregistrationproject.controller;public class IFacade {
+package br.com.ifba.giovaneneves.oopregistrationproject.controller;
+
+import br.com.ifba.giovaneneves.oopregistrationproject.model.Student;
+
+import java.util.List;
+
+public interface IFacade {
+
+    //---------------------------+ Student +--------------------------//
+    boolean saveStudent(Student student);
+    Student findStudentById(int id);
+    boolean removeStudent(int id);
+    boolean updateStudent(Student Student);
+    List<Student> listAllStudents();
+
+
 }
