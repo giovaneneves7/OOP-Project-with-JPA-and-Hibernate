f2de6334c09ee9e3b84bc59c8dd67b2d1a8180be
NekoYasha <autornekoyasha@gmail.com>
2023-04-11 14:12:14 -0300

    added facade layer

--- src/main/java/br/com/ifba/giovaneneves/oopregistrationproject/controller/Facade.java
+++ src/main/java/br/com/ifba/giovaneneves/oopregistrationproject/controller/Facade.java
@@ -1,2 +1,67 @@
-package br.com.ifba.giovaneneves.oopregistrationproject.controller;public class Facade {
+package br.com.ifba.giovaneneves.oopregistrationproject.controller;
+
+//--+ Imports +--//
+import br.com.ifba.giovaneneves.oopregistrationproject.model.Student;
+import br.com.ifba.giovaneneves.oopregistrationproject.service.StudentService;
+
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+//--+ END Imports +--//
+
+@Controller
+public class Facade implements IFacade{
+
+    private final StudentService studentService = new StudentService();
+
+    /**
+     *
+     * Inserts a student int the database
+     * @param student to be added to the database.
+     */
+    @Override
+    public boolean saveStudent(Student student) {
+        return studentService.saveStudent(student);
+    }
+
+    /**
+     *
+     * Search a student in the database
+     * @param id of the student to be searched.
+     * @return student with the specified ID, null otherwise.
+     */
+    @Override
+    public Student findStudentById(int id) {
+        return studentService.findStudentById(id);
+    }
+
+    /**
+     * List all students.
+     * @return a list with all students in the database.
+     */
+    @Override
+    public List<Student> listAllStudents() {
+        return studentService.listAllStudents();
+    }
+
+    /**
+     *
+     * @param id of the student to be removed from the database.
+     * @return true if the student exists, false otherwise.
+     */
+    @Override
+    public boolean removeStudent(int id) {
+        return studentService.removeStudent(id);
+    }
+
+    /**
+     *
+     * @param student to be updated.
+     * @return true if the student exists in the database and the update was successful, false otherwise.
+     */
+    @Override
+    public boolean updateStudent(Student student) {
+        return studentService.updateStudent(student);
+    }
+
 }
