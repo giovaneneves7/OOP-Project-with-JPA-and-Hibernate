9c88d2850dc97306f4c1c0d9a74db79f8a659c54
NekoYasha <autornekoyasha@gmail.com>
2023-04-11 14:19:00 -0300

    added comments

--- src/main/java/br/com/ifba/giovaneneves/oopregistrationproject/controller/IFacade.java
+++ src/main/java/br/com/ifba/giovaneneves/oopregistrationproject/controller/IFacade.java
@@ -7,11 +7,40 @@ import java.util.List;
 public interface IFacade {
 
     //---------------------------+ Student +--------------------------//
+    /**
+     *
+     * Inserts a student int the database
+     * @param student to be added to the database.
+     */
     boolean saveStudent(Student student);
+
+    /**
+     *
+     * Search a student in the database
+     * @param id of the student to be searched.
+     * @return student with the specified ID, null otherwise.
+     */
     Student findStudentById(int id);
-    boolean removeStudent(int id);
-    boolean updateStudent(Student Student);
+
+    /**
+     * List all students.
+     * @return a list with all students in the database.
+     */
     List<Student> listAllStudents();
+
+    /**
+     *
+     * @param id of the student to be removed from the database.
+     * @return true if the student exists, false otherwise.
+     */
+    boolean removeStudent(int id);
+
+    /**
+     *
+     * @param student to be updated.
+     * @return true if the student exists in the database and the update was successful, false otherwise.
+     */
+    boolean updateStudent(Student student);
 
 
 }
