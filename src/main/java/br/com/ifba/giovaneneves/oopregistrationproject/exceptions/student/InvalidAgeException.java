9ba3ad6d3027c9b124177a18a64304f2db665757
NekoYasha <autornekoyasha@gmail.com>
2023-04-13 09:43:53 -0300

    added 'InvalidAgeException'

---
+++ src/main/java/br/com/ifba/giovaneneves/oopregistrationproject/exceptions/student/InvalidAgeException.java
@@ -0,0 +1,8 @@
+package br.com.ifba.giovaneneves.oopregistrationproject.exceptions.student;
+
+public class InvalidAgeException extends Exception{
+
+    public InvalidAgeException(String message){
+        super(message);
+    }
+}
