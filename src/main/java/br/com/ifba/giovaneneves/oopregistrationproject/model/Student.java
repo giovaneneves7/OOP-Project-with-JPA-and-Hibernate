455a9e6babf269f7e9dac14222890dd122266ded
NekoYasha <autornekoyasha@gmail.com>
2023-04-14 11:47:48 -0300

    added 'column' annotation in attributes

--- src/main/java/br/com/ifba/giovaneneves/oopregistrationproject/model/Student.java
+++ src/main/java/br/com/ifba/giovaneneves/oopregistrationproject/model/Student.java
@@ -7,7 +7,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.time.LocalDate;
-        
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 //--+ END Imports +--//
@@ -18,16 +19,27 @@ import javax.persistence.Table;
  */
 @EqualsAndHashCode(callSuper = true)
 @Entity
-@Table (name = "students")
+@Table(name = "students")
 @Data
 public class Student extends AbstractEntity{
     
     //----------------------------------------------{ ATTRIBUTES }----------------------------------------------//
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false, unique = true)
     private String registrationNumber;
+
+    @Column(nullable = false)
     private LocalDate birthDate;
+
+    @Column(nullable = false)
     private int academicYear;
+
+    @Column(nullable = false)
     private float[] grades = new float[3];
+
+    @Column(nullable = false)
     private float averageGrades;
     
     
